a402f856080cbf47a99b09ad983a023369763ac7
Mosesjoseph <mayimela@gmail.com>
2016-10-22 02:02:02 +0200

    cleanup

--- python files/TrafficAnalyzer/traffic/migrations/0001_initial.py
+++ python files/TrafficAnalyzer/traffic/migrations/0001_initial.py
@@ -15,7 +15,25 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('camera', models.CharField(max_length=50)),
-                ('traffic', models.CharField(max_length=100)),
+                ('roadway_name', models.CharField(default=b'name1', max_length=50)),
+                ('latitude', models.FloatField(default=0)),
+                ('longitude', models.FloatField(default=0)),
+                ('traffic', models.IntegerField(default=0)),
+                ('timestamp', models.DateTimeField()),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='traffic_event',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('roadway_name', models.CharField(max_length=50)),
+                ('location', models.CharField(max_length=50)),
+                ('description', models.CharField(max_length=50)),
+                ('direction_of_travel', models.CharField(max_length=50)),
+                ('lanes_affected', models.CharField(max_length=50)),
                 ('timestamp', models.DateTimeField()),
             ],
             options={
--- python files/TrafficAnalyzer/traffic/models.py
+++ python files/TrafficAnalyzer/traffic/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class camera_info(models.Model):
     camera=models.CharField(max_length=50)
-    roadway_name=models.CharField(max_length=50,default='name')
+    roadway_name=models.CharField(max_length=50,default='name1')
     latitude=models.FloatField(default=0)
     longitude=models.FloatField(default=0)
     traffic=models.IntegerField(default=0)
@@ -12,6 +12,7 @@ class camera_info(models.Model):
 
 class traffic_event(models.Model):
     roadway_name=models.CharField(max_length=50)
+    location=models.CharField(max_length=50)
     description=models.CharField(max_length=50)
     direction_of_travel=models.CharField(max_length=50)
     lanes_affected=models.CharField(max_length=50)
